2e0fd7fed14ff324e77c3c8a4946f35595c3d834
omvey <113108807+omvey@users.noreply.github.com>
2025-11-16 21:32:08 +0700

    update bot_nota.py

--- bot_nota.py
+++ bot_nota.py
@@ -934,9 +934,8 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             
             # Tampilkan ringkasan sementara
             total_sementara = sum(item['subtotal'] for item in session['data']['daftar_barang'])
-            summary_text = "*           𝙱𝙾𝚃 𝙼𝙰𝙽𝙰𝙹𝙴𝙼𝙴𝙽 𝙺𝙴𝚄𝙰𝙽𝙶𝙰𝙽        *"
-            summary_text += "*𝗕𝗘𝗥𝗞𝗔𝗛 𝗗𝗨𝗔 𝗣𝗨𝗧𝗥𝗜*\n\n"
-            summary_text += f"Barang ditambahkan :*\n{current_item['nama']}\n{qty} pcs x {format_rupiah(current_item['harga'])} = {format_rupiah(current_item['subtotal'])}\n\n"
+            
+            summary_text = f"Barang ditambahkan :*\n{current_item['nama']}\n{qty} pcs x {format_rupiah(current_item['harga'])} = {format_rupiah(current_item['subtotal'])}\n\n"
             summary_text += f"💰 *Total sementara:* {format_rupiah(total_sementara)}\n\n"
             summary_text += "Pilih opsi di bawah:"
             
